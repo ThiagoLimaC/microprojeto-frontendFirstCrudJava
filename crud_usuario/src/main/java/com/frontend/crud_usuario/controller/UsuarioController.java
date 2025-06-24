c1cb2c69adfb0bbf6d26f90d66be22d90b27c13a
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2025-06-24 16:06:49 -0300

    Adicionar estrutura inicial do projeto com configuração Maven e setup básico da aplicação - Estruturado o projeto com diretórios padrão do Maven (src/main/java, src/test/java, etc.) - Adicionados arquivos do Maven Wrapper (mvnw, mvnw.cmd) e pasta .mvn para facilitar a execução em diferentes ambientes - Criado o arquivo pom.xml com dependências básicas necessárias (como spring-boot-starter) - Implementada a classe principal de aplicação com anotação @SpringBootApplication para inicialização automática - Criado o arquivo application.properties com configurações iniciais da aplicação

---
+++ crud_usuario/src/main/java/com/frontend/crud_usuario/controller/UsuarioController.java
@@ -0,0 +1,8 @@
+package com.frontend.crud_usuario.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UsuarioController {
+    
+}
---
+++ crud_usuario/src/main/java/com/frontend/crud_usuario/model/Usuario.java
@@ -0,0 +1,64 @@
+package com.frontend.crud_usuario.model;
+
+import java.time.LocalDate;
+
+public class Usuario {
+    
+    ////#region Atributos
+
+    private Integer id;
+
+    private String nome;
+
+    private String telefone;
+
+    private String email;
+
+    private LocalDate dataNascimento;
+
+    //#endregion
+
+
+    //#region Getters and Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    //#endregion
+}
