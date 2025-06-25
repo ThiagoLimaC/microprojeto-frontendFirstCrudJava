d35ef1e8c1804737957d6ec3887efa3ce495e936
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2025-06-24 21:33:47 -0300

    Adicionar funcionalidades de cadastro e edição de usuários com templates Thymeleaf

--- crud_usuario/src/main/java/com/frontend/crud_usuario/controller/UsuarioController.java
+++ crud_usuario/src/main/java/com/frontend/crud_usuario/controller/UsuarioController.java
@@ -4,11 +4,17 @@ import java.util.Arrays;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.frontend.crud_usuario.model.Usuario;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
@@ -24,4 +30,45 @@ public class UsuarioController {
         mv.addObject("usuarios", Arrays.asList(usuarios));
         return mv;
     }
+
+    @GetMapping("/retornarUsuarios/novo")
+    public ModelAndView mostrarFormularioCadastro(){
+        ModelAndView mv = new ModelAndView("cadastrar");
+        mv.addObject("usuario", new Usuario());
+        return mv;
+    }
+
+    @PostMapping("/novoUsuario")
+    public String cadastrarUsuario(@ModelAttribute Usuario usuario) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:8083/api/usuarios";
+
+        restTemplate.postForEntity(url, usuario, Void.class);
+        
+        return "redirect:/retornarUsuarios";
+    }
+    
+    @GetMapping("/retornarUsuarios/editar/{id}")
+    public ModelAndView mostrarFormularioEdicao(@PathVariable Integer id){
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:8083/api/usuarios/" + id;
+
+        Usuario usuario = restTemplate.getForObject(url, Usuario.class);
+
+        ModelAndView mv = new ModelAndView("editar");
+        mv.addObject("usuario", usuario);
+        return mv;
+    }
+
+    @PostMapping("/editarUsuario")
+    public String editarUsuario(@ModelAttribute Usuario usuario) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:8083/api/usuarios/" + usuario.getId();
+
+        restTemplate.put(url, usuario);
+
+        return "redirect:/retornarUsuarios";
+    }
+    
+
 }
